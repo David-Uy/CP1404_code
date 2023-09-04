8e0b8634ad44eb006f4fd8878c704a49e93af471
David_P2 <daviduy2003@gmail.com>
2023-09-04 14:23:44 +0800

    Update project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,7 +2,7 @@ import datetime
 
 
 class Project:
-    def __init__(self, name="", start_date="", priority=int, cost_estimate=float, completion_percentage=float):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -10,7 +10,8 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate:,.2f}, {self.completion_percentage}"
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: {self.cost_estimate:,.2f}, " \
+               f"completion: {self.completion_percentage}"
 
     def __repr__(self):
         return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate:,.2f}, {self.completion_percentage}"
