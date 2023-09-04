13e14fceb5621e7d4f461616c348c94109f4a676
David_P2 <daviduy2003@gmail.com>
2023-09-04 12:24:07 +0800

    modify programming language and change position of pointer_arithmetic

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -2,39 +2,30 @@ class ProgrammingLanguage:
     """Represent information about a programming language."""
 
     def __init__(self, name, typing, reflection, year, pointer_arithmetic=False):
-        """
-        Construct a ProgrammingLanguage from the given values.
-
-        :param name: The name of the programming language.
-        :param typing: The typing system of the language (e.g., 'Dynamic' or 'Static').
-        :param reflection: True if the language supports reflection, False otherwise.
-        :param year: The year when the language first appeared.
-        :param pointer_arithmetic: True if the language supports pointer arithmetic, False otherwise.
-        """
+        """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
-        self.year = year
         self.pointer_arithmetic = pointer_arithmetic
+        self.year = year
 
-    def __repr__(self):
-        """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer Arithmetic={self.pointer_arithmetic}"
+
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
     def __str__(self):
-        """Return a user-friendly string representation of a ProgrammingLanguage."""
-        pointer_info = "Supports Pointer Arithmetic" if self.pointer_arithmetic else "Does not support Pointer Arithmetic"
-        return f"Name: {self.name}\nTyping: {self.typing}\nReflection: {self.reflection}\nYear: {self.year}\n{pointer_info}"
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, " \
+               f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}"
+
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, True)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, False, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, False, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, True, 1991)
 
     languages = [ruby, python, visual_basic]
     print(python)
@@ -43,6 +34,7 @@ def run_tests():
     for language in languages:
         if language.is_dynamic():
             print(language.name)
+
 
 if __name__ == "__main__":
     run_tests()
