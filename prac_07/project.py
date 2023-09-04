97be88805585f17846ef2db081424304b8c2d7d3
David_P2 <daviduy2003@gmail.com>
2023-09-04 13:43:02 +0800

    project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,22 +2,32 @@ import datetime
 
 
 class Project:
-    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+    def __init__(self, name="", start_date="", priority=int, cost_estimate=float, completion_percentage=float):
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, __format="%d/%m/%y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        return f"Name: {self.name}, Start Date: {self.start_date}, Priority: {self.priority}, " \
-               f"Cost Estimate: {self.cost_estimate}, Completion Percentage: {self.completion_percentage}%"
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate:,.2f}, {self.completion_percentage}"
+
+    def __repr__(self):
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate:,.2f}, {self.completion_percentage}"
 
     def is_completed(self):
-        return self.completion_percentage == 100
+        return int(self.completion_percentage) == 100
 
-    def days_until_completion(self):
-        if self.is_completed():
-            return 0
-        today = datetime.date.today()
-        return (self.start_date - today).days
+    def __lt__(self, other):
+        return self.priority <= other.priortiy
+
+    def compare_date(self, new_date):
+        new_date = datetime.datetime.strptime(new_date, __format="%d/%m/%y").date()
+        return self.start_date >= new_date
+
+    def update_percentage(self, new_percent):
+        self.completion_percentage = new_percent
+
+    def update_priority(self, value):
+        self.priority = value
+
